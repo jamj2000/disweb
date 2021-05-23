ec8fe6b7f2405192903ca6b53bd838b69a0cb2c8
juanmaferv96 <juanma.campanero96@gmail.com>
2021-05-23 18:40:34 +0200

    Modificando archivo offline intento de poner imagen de fondo

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,7 @@ const CACHE_NAME = 'infocovid-v1';
 const CACHE_ASSETS = [
   '/',
   '/index.html',
-  '/offline.html',
+  '/offline2.html',
   '/favicon.png',
   '/global.css',
   '/build/bundle.css',
@@ -80,7 +80,7 @@ self.addEventListener('fetch', function (e) {
       .catch(() => {
         return caches.open(CACHE_NAME)
           .then((cache) => {
-            return cache.match('/offline.html');
+            return cache.match('/offline2.html');
           });
       })
   );
